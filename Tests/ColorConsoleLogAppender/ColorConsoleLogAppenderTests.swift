c7608c9bca643d2666230a9f0f67d2d8d8038a2a
i.ushakov <i.ushakov@perpetuumlab.com>
2016-08-23 15:13:47 +0400

    init

---
+++ Tests/ColorConsoleLogAppender/ColorConsoleLogAppenderTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import ColorConsoleLogAppender
+
+class ColorConsoleLogAppenderTests: XCTestCase {
+
+	func testExample() {
+		// This is an example of a functional test case.
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	}
+
+}
+extension ColorConsoleLogAppenderTests {
+	static var allTests : [(String, ColorConsoleLogAppenderTests -> () throws -> Void)] {
+		return [
+			("testExample", testExample),
+		]
+	}
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import ColorConsoleLogAppenderTestSuite
+
+XCTMain([
+	 testCase(ColorConsoleLogAppenderTests.allTests),
+])
